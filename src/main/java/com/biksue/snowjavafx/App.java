6247d553d23900d995fe735ec27c2b59fcea5194
Suehay <117776704+Bikoru-Suehay@users.noreply.github.com>
2023-02-08 11:02:39 +0100

    Change 1.1: -Create Changes in archives -Proved maven shade plugin

---
+++ src/main/java/com/biksue/snowjavafx/App.java
@@ -0,0 +1,9 @@
+package com.biksue.snowjavafx;
+
+import javafx.application.Application;
+
+public class App {
+    public static void main(String[] args) {
+        Application.launch(SnowJavaFx.class,args);
+    }
+}
